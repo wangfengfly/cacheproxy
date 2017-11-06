0879922d45d391f8f4c786b0b994aba773703485
wangfeng <985004673@qq.com>
2017-11-06 17:18:02 +0800

    根据key的多少决定计算方式

--- CaculateKeySize.php
+++ CaculateKeySize.php
@@ -10,42 +10,48 @@ class CaculateKeySize{
 
     public function run(){
         $command = 'redis-cli -h a.redis.sogou -p 1976 -a hahaunimportant';
-        $keys = shell_exec($command.' scan 0');
-        $keys = explode("\n", $keys);
-        $total = 0;
-        $content = array();
-        while(count($keys)>0 && $keys[0]!=0){
-            $cursor = $keys[0];
-            $keysn = count($keys);
-            for($i=1; $i<=$keysn; $i++){
-                $key = $keys[$i];
-                if($key){
-                    $ret = shell_exec($command." debug object $key");
-                    $ret = explode(' ', $ret);
-                    $size = explode(':', $ret[4]);
-                    $total += $size[1];
-                    $content[$key] = $size[1];
-                }
-            }
-            $keys = shell_exec($command." scan $cursor");
+        $dbsize = shell_exec($command.' dbsize');
+        if($dbsize > self::MAX_KEYS_LIMIT){
+            $str = shell_exec($command.' --bigkeys -i 0.3');
+            $head = str_replace("\n", "<br>", $str);
+        }else {
+            $keys = shell_exec($command . ' scan 0');
             $keys = explode("\n", $keys);
-        }
+            $total = 0;
+            $content = array();
+            while (count($keys) > 0 && $keys[0] != 0) {
+                $cursor = $keys[0];
+                $keysn = count($keys);
+                for ($i = 1; $i <= $keysn; $i++) {
+                    $key = $keys[$i];
+                    if ($key) {
+                        $ret = shell_exec($command . " debug object $key");
+                        $ret = explode(' ', $ret);
+                        $size = explode(':', $ret[4]);
+                        $total += $size[1];
+                        $content[$key] = $size[1];
+                    }
+                }
+                $keys = shell_exec($command . " scan $cursor");
+                $keys = explode("\n", $keys);
+            }
 
-        arsort($content);
-        $str = '';
-        $head = '';
-        $i=0;
-        foreach($content as $key=>$size){
-            if($size) {
-                $line = $key . ":\t" . $size . " Bytes\n";
-                $str .= $line;
-                if ($i <= 50) {
-                    $head .= $line . "<br>";
-                    $i++;
+            arsort($content);
+            $str = '';
+            $head = '';
+            $i = 0;
+            foreach ($content as $key => $size) {
+                if ($size) {
+                    $line = $key . ":\t" . $size . " Bytes\n";
+                    $str .= $line;
+                    if ($i <= 50) {
+                        $head .= $line . "<br>";
+                        $i++;
+                    }
                 }
             }
         }
-        $filename = md5(trim($host).$passwd);
+        $filename = md5(trim($host) . $passwd);
         file_put_contents("/tmp/$filename.csv", $str);
     }
 
